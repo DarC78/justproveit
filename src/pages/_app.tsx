88ec640c7359ae92ee80c320d18d90825b86739a
Adrian D <adrian@proveitweb.co.uk>
2026-07-20 07:39:10 +0300

    Install Microsoft Clarity tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,24 @@
 import "@/styles/globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
+      <Script
+        id="microsoft-clarity"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "xp8gw8yl07");
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </AuthProvider>
   );
